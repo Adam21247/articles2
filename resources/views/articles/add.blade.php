4ba7d99650eea17a11936cfa04390e2609fbb2d9
AdamOrzech <adamorzech92@gmail.com>
2022-10-02 13:15:25 +0200

    created edit.css file and I have made changes in edit.blade.php

--- resources/views/articles/add.blade.php
+++ resources/views/articles/add.blade.php
@@ -2,13 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 
-
 <head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" type="text/css" href="{{ url('/css/add.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/add.css') }}"/>
 
 </head>
 
@@ -20,23 +19,23 @@
 </a>
 
 <div class="addMember-box">
-<form action="{{route(('articles.create'))}}" method="POST">
-    <h1>Add Article</h1>
+    <form action="{{route(('articles.create'))}}" method="POST">
+        <h1>Add Article</h1>
 
-    <p></p>
-    @csrf
+        <p></p>
+        @csrf
 
 
         <input type="text" name="title" placeholder="Enter title">
 
 
-    <textarea name="summary" placeholder="Enter summary"></textarea>
+        <textarea name="summary" placeholder="Enter summary"></textarea>
 
 
-    <textarea name="content" placeholder="Enter content"></textarea>
+        <textarea name="content" placeholder="Enter content"></textarea>
 
-    <button type="submit">Add New Article</button>
-</form>
+        <button type="submit">Add New Article</button>
+    </form>
 
 </div>
 </body>
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,11 +1,24 @@
-<h1>Update data</h1>
-<button><a href="{{"/articles"}}">Back</a></button>
-<p></p>
-<form action="{{route('articles.update')}}" method="POST">
-    @csrf
-    <input type="hidden" name="id" value="{{$article['id']}}">
-    <input type="text" name="title" value="{{$article['title']}}"><br>
-    <input type="text" name="summary" value="{{$article['summary']}}"><br>
-    <input type="text" name="content" value="{{$article['content']}}"><br>
-    <button type="submit">Update</button>
-</form>
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/edit.css') }}"/>
+</head>
+<a href="{{"/articles"}}">
+    <button> Back</button>
+</a>
+
+
+<div class="editMember-box">
+    <h1>Update data</h1>
+
+    <form action="{{route('articles.update')}}" method="POST">
+        @csrf
+        <input type="hidden" name="id" value="{{$article['id']}}">
+        <input type="text" name="title" value="{{$article['title']}}"><br>
+        <textarea type="text" name="summary">{{$article['summary']}} </textarea>
+        <textarea type="text" name="content">{{$article['content']}} </textarea>
+        <button type="submit">Update</button>
+    </form>
+</div>
+
+
+
+
